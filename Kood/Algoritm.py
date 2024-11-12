eb88cd87816d15f48ac16f2531ca8dfd7c7336cd
TheGreyCore <kasparparkseppp@gmail.com>
2024-11-12 21:24:10 +0200

    lolololol

--- Kood/Algoritm.py
+++ Kood/Algoritm.py
@@ -1,3 +1,23 @@
 
 
-print("tere")
+def algaja(kaal, vanus, pikkus, siruulatus):
+    if kaal < 210 and if kaal > 50:
+        if kaal >= 130:
+            kaal_pkt = (210 - kaal) / 2
+        elif kaal < 130:
+            kaal_pkt = (50 - kaal) / 2 * -1
+    else: 
+        print("Kaal ebareaalne!")
+    if vanus <= 28:
+        vanus_pkt = 80
+    else: 
+        vanus_pkt = 80 - ((vanus - 28) * 0.025)
+        if vanus_pkt < 0:
+            vanus_pkt = 0
+    if pikkus >= 198:
+        pikkus_pkt = 100
+    elif pikkus < 198:
+        pikkus_pkt = 
+    if siruulatus >= 213:
+        siruulatus_pkt = 110
+    elif 
