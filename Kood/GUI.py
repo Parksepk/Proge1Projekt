587cb5eec9f67525d379c497b2170bf5cd5f9bea
hanner100 <hannes.remmelgas@gmail.com>
2024-11-12 21:17:55 +0200

    1

--- Kood/GUI.py
+++ Kood/GUI.py
@@ -6,10 +6,8 @@ root.geometry("500x500")
 root.title("Kettaheite projekt")
 
 label = tk.Label(root, text="Kettaheite potentsiaali kalkulaator", font = ("Arial", 18))
-label.pack(padx=20, pady=20)
+label.pack(padx=20, pady=160)
 
-textbox = tk.Text(root, height=3, font=("Arial", 18))
-textbox.pack(padx=10)
 
 button = tk.Button(root, text = "Alusta!", font=("Arial", 18))
 button.pack()
